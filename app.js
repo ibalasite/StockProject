27ebe8a9c4b16d04ef3f039502a4b3e844590e10
evans tseng <ibalasite@gmail.com>
2018-05-21 23:57:16 +0000

    modify app.js

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
+let config = {}
+async function init(){
 var thrift = require('thrift');
 var configService = require('./gen-nodejs/ConfigService');
-
 var transport = thrift.TBufferedTransport;
 var protocol = thrift.TBinaryProtocol;
-
 var connection = thrift.createConnection("localhost", 9090, {
   transport : transport,
   protocol : protocol
 });
-
 connection.on('error', function(err) {
     console.log("error:"+err);
 });
-
 var client = thrift.createClient(configService, connection);
 const {promisify} = require('util');
 const getConfig = promisify(client.getConfig).bind(client);
-/*
-client.getConfig(function(err, message) {
-    if(err) console.log("error:" + err);
-     var configs = JSON.parse(message);
-     console.log('config is  %j', configs);
-     var RedisServer = JSON.parse(configs["/RedisServer"]);
-     console.log(RedisServer.list);
-     console.log('RedisServer.list length %d',RedisServer.list.length);
-     console.log('RedisServer.list last value ',RedisServer.list[RedisServer.list.length-1]);
-     var DB = JSON.parse(configs["/DB"]);
-     console.log('DB user  %s',DB["user"]);
-     console.log('DB password  %s',DB["password"]);
-  });
-*/
-
-async function config(){
   var message = await getConfig();
-  console.log(message);
+  console.log("message:"+message);
+  console.log("end");
+  connection.end();
+  
 }
+function app()
+{
 
-config();
-console.log("end");
+
+}
+init();
+
