504deb54719807813812e00c6707b35aed1b54b2
evans tseng <ibalasite@gmail.com>
2018-05-22 06:06:43 +0000

    try to test

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 var config= require('./config');
+var redis = require('redis');
 config.init().then(
-(config)=>{
+(configs)=>{
+var RedisServer = JSON.parse(configs["/RedisServer"]);
+console.log("config"+RedisServer.list)
+var HashRing = require('hashring');
+var hosts = RedisServer.list;
+var ring = new HashRing(hosts
+  , 'md5', {
+    'max cache size': 10000
+  });
 
-console.log("config"+config["/DB"])}
+var serverInfo=ring.get('id10001').split(":"); 
+var server = {host:serverInfo[0],port:serverInfo[1]};
+var client = redis.createClient(server.port, server.host);
+client.on('connect', function() {
+    console.log('connected');
+});
+client.on("error", function(error) {
+    console.log(error);
+});
+client.incr("test", function(err, reply) {
+    if (err) throw err;
+    console.log(reply); // 11
+   client.quit();
+});
 
+});
+/*
+var port=config.Redis.Port; 
+var host=config.Redis.Host; 
+var client = redis.createClient(port, host);
 );
+*/
 
+
+/*
+function INCR(key){
+client.on("error", function(error) {
+    console.log(error);
+});
+//client.on('connect', function() {
+//    console.log('connected');
+//});
+client.on("error", function(error) {
+    console.log(error);
+});
+client.incr(key, function(err, reply) {
+    if (err) throw err;
+    console.log(reply); // 11
+});
+client.quit();
+*/
